5bbc653b7e13f61a49ced496f95794b58284981e
Aaro Saila <aaros.saila@gmail.com>
2025-02-15 23:33:58 +0200

    updated compile script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -Wall -Werror -pedantic -o sanke src/main.c src/snake/**.c src/utils/**.c
+gcc -Wall -Werror -pedantic -o sanke src/main.c src/snake/**.c src/utils/**.c src/board/**.c
