c69b5ec8a2ae39464b12831bd6b4fd8d8ce99fec
Aaro Saila <aaros.saila@gmail.com>
2025-05-16 12:20:01 +0300

    Added debug flags

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -Wall -Werror -pedantic -o build/sanke src/main.c src/snake/**.c src/utils/**.c src/board/**.c
+gcc -Og -g -Wall -Werror -pedantic -o build/sanke src/main.c src/snake/**.c src/utils/**.c src/board/**.c
